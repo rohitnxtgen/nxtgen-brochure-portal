65c2d2f0169c67673f12d693d183192916758fb7
rohitnxtgen <rohit.kale@nxtgen.com>
2026-07-28 17:07:28 +0530

    Restore working EmailJS service after validation failure

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.NXTGEN_CONFIG = {
   leadEndpoint: "https://formspree.io/f/mgogdkey",
-  emailJsServiceId: "service_ycietia",
-  emailJsTemplateId: "template_zw4ytb8",
-  emailJsPublicKey: "X_pHQuAOZbDENpxEq",
+  emailJsServiceId: "service_mlps8vn",
+  emailJsTemplateId: "template_5nw1vgj",
+  emailJsPublicKey: "I5nF3hMLr9VxsOEeW",
   senderMailbox: "marketing@nxtgen.com",
   publicDemo: false,
   downloadDelivery: true
