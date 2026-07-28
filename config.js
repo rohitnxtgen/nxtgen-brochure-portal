c5072a946a4ce75fa837d6e219af8aeb6259f688
rohitnxtgen <rohit.kale@nxtgen.com>
2026-07-28 17:10:48 +0530

    Configure corrected EmailJS brochure delivery

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.NXTGEN_CONFIG = {
   leadEndpoint: "https://formspree.io/f/mgogdkey",
-  emailJsServiceId: "service_mlps8vn",
-  emailJsTemplateId: "template_5nw1vgj",
-  emailJsPublicKey: "I5nF3hMLr9VxsOEeW",
+  emailJsServiceId: "service_ycieita",
+  emailJsTemplateId: "template_zw4ytb8",
+  emailJsPublicKey: "X_pHQuAOZbDENpxEq",
   senderMailbox: "marketing@nxtgen.com",
   publicDemo: false,
   downloadDelivery: true
